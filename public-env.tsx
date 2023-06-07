41a74a1bce9571d87d2c87b843e89b0818ae9588
twotimesgi <luigibardellagerbi@mail.com>
2023-06-07 10:51:18 +0200

    fix: added env vars

--- public-env.tsx
+++ public-env.tsx
@@ -1,17 +1,30 @@
 let CONTRACT_ADDR : string;
 let EXPLORER : string;
-
-if(!process.env.NEXT_PUBLIC_CONTRACTADDR){
+let INFURA_IPFS_PROJECTID : string;
+let INFURA_IPFS_SECRET: string;
+if(!process.env.NEXT_PUBLIC_CONTRACTADDR || process.env.NEXT_PUBLIC_CONTRACTADDR == ""){
     throw new Error("process.env.NEXT_PUBLIC_CONTRACTADDR is undefined")
 }else{
     CONTRACT_ADDR = process.env.NEXT_PUBLIC_CONTRACTADDR;
 }
 
-if(!process.env.NEXT_PUBLIC_EXPLORER){
+if(!process.env.NEXT_PUBLIC_EXPLORER || process.env.NEXT_PUBLIC_EXPLORER == ""){
     throw new Error("process.env.NEXT_PUBLIC_EXPLORER is undefined")
 }else{
     EXPLORER = process.env.NEXT_PUBLIC_EXPLORER;
 }
 
+if(!process.env.NEXT_PUBLIC_INFURA_IPFS_PROJECTID || process.env.NEXT_PUBLIC_INFURA_IPFS_PROJECTID == ""){
+    throw new Error("process.env.NEXT_PUBLIC_PROJECTID is undefined")
+}else{
+    INFURA_IPFS_PROJECTID = process.env.NEXT_PUBLIC_EXPLORER;
+}
 
-export {CONTRACT_ADDR, EXPLORER}
+if(!process.env.NEXT_PUBLIC_INFURA_IPFS_SECRET || process.env.NEXT_PUBLIC_INFURA_IPFS_SECRET == ""){
+    throw new Error("process.env.NEXT_PUBLIC_INFURA_IPFS_SECRET is undefined")
+}else{
+    INFURA_IPFS_SECRET = process.env.NEXT_PUBLIC_INFURA_IPFS_SECRET;
+}
+
+
+export {CONTRACT_ADDR, EXPLORER, INFURA_IPFS_PROJECTID, INFURA_IPFS_SECRET}
